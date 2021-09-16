20d463b46a25b92a020b36fba46af5654ae956a9
oddbary <38177004+oddbary@users.noreply.github.com>
2021-09-17 00:11:33 +0900

    Create jquery.js

---
+++ jquery.js
@@ -0,0 +1,31 @@
+$(function(){
+
+//ヘッダーアコーディオンの処理
+
+    $(".header-hiphop").on("click", function() {
+        $(".header-hiphop-list").slideToggle();
+    });
+
+    $(".header-jpop").on("click", function() {
+        $(".header-jpop-list").slideToggle();
+    });
+
+    $(".header-vocaloid").on("click", function() {
+        $(".header-vocaloid-list").slideToggle();
+    });
+
+
+//レスポンシブメニューの高さ調整
+
+    var height=$("#header").height();
+    $(".responsive-menu").css("margin-top", height - 21);
+
+
+//ハンバーガー開閉時の処理
+
+    $(".btn-header").on('click', function() {
+        $('.responsive-menu,.btn-line').toggleClass('open');
+    });
+
+
+});
