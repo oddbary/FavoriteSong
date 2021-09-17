d4bae416adfccda5b5c2ac329953e029a0c90063
oddbary <38177004+oddbary@users.noreply.github.com>
2021-09-17 21:58:53 +0900

    Create favorite.js
    アコーディオン開閉

---
+++ favorite.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded",() => {
+    const title = document.querySelectorAll('.responsive-title');
+
+    for(let i= 0; i < title.length; i++) {
+        let titleEach = title[i];
+        let content = titleEach.nextElementSibling;
+        titleEach.addEventListener('click',()=> {
+            titleEach.classList.toggle('active');
+            content.classList.toggle('open');
+        });
+    }
+});
+
+document.addEventListener("DOMContentLoaded",() => {
+    const footertitle = document.querySelectorAll('.footer-title');
+
+    for(let i= 0; i < footertitle.length; i++) {
+        let footertitleEach = footertitle[i];
+        let footercontent =footertitleEach.nextElementSibling;
+        footertitleEach.addEventListener('click',() => {
+            footertitleEach.classList.toggle('active');
+            footercontent.classList.toggle('open');
+        });
+    }
+});
